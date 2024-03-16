bb0cefe0adc0a6528a01a080fd43a2f5fb7ff348
Krushnal <krushnalpatil2018@gmail.com>
2024-03-16 22:27:28 +0530

    20

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,36 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Hide the loader container after the page has loaded
-    document.getElementById('loader-container').style.display = 'none';
+  // Immediately show the loader when the DOM starts loading
+  showLoader();
+
+  // Hide the loader once the page is fully loaded
+  window.addEventListener('load', function() {
+    hideLoader();
   });
 
+  // Additionally, in case the load event has already been fired before the script runs
+  if (document.readyState === 'complete') {
+    hideLoader();
+  } else {
+    // If the page is still loading, ensure the loader is hidden once it's done
+    window.addEventListener('load', hideLoader);
+  }
+});
 
-  window.addEventListener('beforeunload', function() {
-    // Show the loader container before the page is unloaded
-    document.getElementById('loader-container').style.display = 'flex';
-  });
+function showLoader() {
+  var loaderContainer = document.getElementById('loader-container');
+  if (loaderContainer) {
+    loaderContainer.style.display = 'flex'; // Assuming 'flex' is your initial display type
+  }
+}
+
+function hideLoader() {
+  // Use setTimeout to ensure the loader is displayed for a minimum of 3 seconds
+  setTimeout(function() {
+    var loaderContainer = document.getElementById('loader-container');
+    if (loaderContainer) {
+      loaderContainer.style.display = 'none';
+    }
+  }, 3000); // Adjust time as needed (3000ms = 3 seconds)
+}
+
